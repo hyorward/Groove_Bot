985153716a88168a97961df593ea20353e45b6cd
wh1telqd <wh1telqd@gmail.com>
2025-03-01 05:42:56 +0300

    Bot stays in channel

--- bot/bot.py
+++ bot/bot.py
@@ -1,8 +1,13 @@
-from discord import Intents
+from discord import Intents, opus
 from discord.ext import commands
+
+from bot import config
 
 intents = Intents.default()
 intents.message_content = True
 intents.voice_states = True  # Обработка голосовых состояний
 
 bot_instance = commands.Bot(command_prefix='/', intents=intents)
+
+if config.USING_OPUS:
+    opus.load_opus(config.OPUS_PATH)
