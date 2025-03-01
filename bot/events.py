59c3b488105df2f196c1ffbada142afe1e482523
Abdulmalik Maxudov <maxudov.ar@getflow.tech>
2025-03-02 00:38:47 +0300

    At least say meow yelki-palki sound

--- bot/events.py
+++ bot/events.py
@@ -1,15 +1,17 @@
 import asyncio
 import os
 import random
+import time
 
 import discord
-from discord import VoiceState
+from discord import VoiceState, VoiceClient
 
 from bot.bot import bot_instance
 from bot.config import AUDIO_FOLDER
 from bot.prayers import check_prayer_times
 
 ON_JOIN_AUDIOS = ['mephala.mp3', 'bratishka.mp3']
+last_time_played_meow = None
 
 
 @bot_instance.event
@@ -19,24 +21,13 @@ async def on_ready():
     print("Бот запущен.")
 
 
-@bot_instance.event
-async def on_voice_state_update(member, before_voice_state: VoiceState, after_voice_state: VoiceState):
-    if before_voice_state.channel == after_voice_state.channel:
-        return
-    if not after_voice_state.channel and before_voice_state.channel:  # если все вышли бот тоже выходит
-        members = before_voice_state.channel.members
-        if (len(members) == 1) and (members[0].id == bot_instance.user.id):
-            await member.guild.voice_client.disconnect()
+async def leave(member, before_voice_state: VoiceState):
+    members = before_voice_state.channel.members
+    if (len(members) == 1) and (members[0].id == bot_instance.user.id):
+        await member.guild.voice_client.disconnect()
 
-    if not after_voice_state.channel:  # Если пользователь не зашел в голосовой канал
-        return
 
-    voice_channel = after_voice_state.channel
-    if not voice_channel:  # Проверка на существование канала
-        return
-
-    random_audio = random.choice(ON_JOIN_AUDIOS)  # Выбираем случайный файл
-
+async def _get_voice_client(member, voice_channel) -> VoiceClient | None:
     # Если бот еще не находится в новом канале
     if bot_instance.user.id not in [user.id for user in voice_channel.members]:
         vc = member.guild.voice_client
@@ -50,8 +41,77 @@ async def on_voice_state_update(member, before_voice_state: VoiceState, after_vo
             print("иногда случается надо разобраться почему")
             return
         vc = member.guild.voice_client
-    audio_path = os.path.join(AUDIO_FOLDER, random_audio)
-    vc.play(discord.FFmpegPCMAudio(audio_path))  # Воспроизведение случайного аудиофайла
+    return vc
 
-    while vc.is_playing():
+
+async def greetings(member, voice_channel):
+    voice_client = await _get_voice_client(member, voice_channel)
+    random_audio = random.choice(ON_JOIN_AUDIOS)  # Выбираем случайный файл
+
+    audio_path = os.path.join(AUDIO_FOLDER, random_audio)
+    voice_client.play(
+        discord.FFmpegPCMAudio(audio_path))  # Воспроизведение случайного аудиофайла
+
+    while voice_client.is_playing():
         await asyncio.sleep(1)
+
+
+async def meow(member, voice_channel):
+    voice_client = await _get_voice_client(member, voice_channel)
+
+    audio_path = os.path.join(AUDIO_FOLDER, "meow.mp3")
+    voice_client.play(discord.FFmpegPCMAudio(audio_path))
+
+    while voice_client.is_playing():
+        await asyncio.sleep(1)
+
+
+def _get_muted_members(bot_id, voice_state: VoiceState):
+    return [
+        (member.voice.mute or member.voice.self_mute) for member in voice_state.channel.members if
+        member.id != bot_id
+    ]
+
+
+def muted_right_now(before, after):
+    return not (before.self_mute or before.mute) and (after.self_mute or after.mute)
+
+
+def throttle_meow():
+    global last_time_played_meow
+    if last_time_played_meow is None:
+        last_time_played_meow = time.time()
+        return True
+    if (time.time() - last_time_played_meow) > 15:
+        last_time_played_meow = time.time()
+        return True
+    return False
+
+
+@bot_instance.event
+async def on_voice_state_update(member, before: VoiceState, after: VoiceState):
+    if member.id == bot_instance.user.id:
+        return
+    if (
+            all((after, before))
+            and (
+                all(_get_muted_members(bot_instance.user.id, after))
+                and
+                muted_right_now(before, after)
+            )
+            and throttle_meow()
+    ):
+        await asyncio.sleep(5)
+        await meow(member, after.channel)
+        return
+    if before.channel == after.channel:
+        #  Далее ивенты только для входа/выхода юзера из канала
+        return
+    if not after.channel and before.channel:  # если все вышли бот тоже выходит
+        await leave(member, before)
+        return
+
+    if not (
+            voice_channel := after.channel):  # Если пользователь не зашел в голосовой канал
+        return
+    await greetings(member, voice_channel)
