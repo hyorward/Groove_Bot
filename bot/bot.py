3f9c6373abc9db700d6f399dda1c5d1cb33c2945
hyorward <arturhyorward@gmail.com>
2026-01-12 13:57:50 -0800

    Add SOCKS5 proxy support and configure TCP connector for Discord bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,3 +1,4 @@
+import aiohttp
 from discord import Intents, opus
 from discord.ext import commands
 
@@ -9,7 +10,15 @@ intents.voice_states = True
 intents.members = True  # Нужен для проверки участников канала
 intents.guilds = True
 
-bot_instance = commands.Bot(command_prefix='/', intents=intents)
+# Настройка SOCKS5 прокси для обхода блокировок Discord voice
+PROXY_URL = "socks5://127.0.0.1:10808"
+
+connector = aiohttp.TCPConnector(limit=100)
+bot_instance = commands.Bot(
+    command_prefix='/',
+    intents=intents,
+    proxy=PROXY_URL
+)
 
 if config.USING_OPUS:
     opus.load_opus(config.OPUS_PATH)
